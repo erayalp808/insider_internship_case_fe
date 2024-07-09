2c938b3f6a3cd611a0c018397a50f8902395b85f
Eray Alp <erayalp.e@gmail.com>
2024-07-09 13:20:42 +0300

    install fontawesome

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import store from './store'
-import './assets/tailwind.css'
+import { createApp } from "vue";
+import App from "./App.vue";
+import store from "./store";
+import "./assets/tailwind.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { fas } from "@fortawesome/free-solid-svg-icons";
 
-createApp(App).use(store).mount('#app')
+library.add(fas);
+
+createApp(App)
+  .use(store)
+  .component("font-awesome-icon", FontAwesomeIcon)
+  .mount("#app");
